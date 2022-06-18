ffcff780c2b337f0426b47f74545dfb7f376a7fe
RenzoNogueira <mainrenzossn@gmail.com>
2022-06-18 00:58:25 -0300

    Frameworks instaldos via npm

--- src/main.js
+++ src/main.js
@@ -2,4 +2,16 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+// Bootstrap
+import 'bootstrap/dist/css/bootstrap.min.css'
+import bootstrap from 'bootstrap/dist/js/bootstrap.min'
+window.bootstrap = bootstrap
+
+// Popper
+import '@popperjs/core/dist/umd/popper.js'
+
+// Jquery
+const $ = require('jquery')
+window.$ = $
+
 createApp(App).use(router).mount('#app')
