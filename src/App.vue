ba5077a5440be1b75e652b014d6e1152c3c15c0a
RenzoNogueira <mainrenzossn@gmail.com>
2022-06-18 18:12:25 -0300

    Layout inicial feito

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,12 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
+<div class="bg-dark">
+  <nav class="bg-dark text-white py-0 px-3 text-start">
+    <span>Wanon</span>
+  </nav>
   <router-view/>
+</div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<style lang="scss" scoped>
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
